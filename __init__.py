b35d9b62df2f8e4a98045a3fae161940c224ef38
theresa chan <theresachan36@gmail.com>
2021-01-04 22:49:19 +0800

    first commiting code

---
+++ __init__.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from Forms import CreateUserForm
+import shelve, User
+
+app = Flask(__name__)
+app.secret_key = 'any_random_string'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/aboutus")
+def blog():
+    return render_template('about.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('cart.html')
+
+@app.route("/checkout")
+def checkout():
+    return render_template('checkout.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/shopgrid")
+def shopgrid():
+    return render_template('shop_grid.html')
+
+@app.route('/createUser', methods=['GET', 'POST'])
+def create_user():
+    create_user_form = CreateUserForm(request.form)
+    if request.method == 'POST' and create_user_form.validate():
+        users_dict = {}
+        db = shelve.open('storage.db', 'c')
+
+        try:
+            users_dict = db['Users']
+        except:
+            print("Error in retrieving Users from storage.db.")
+
+        user = User.User(create_user_form.first_name.data, create_user_form.last_name.data, create_user_form.gender.data, create_user_form.membership.data, create_user_form.remarks.data)
+        users_dict[user.get_user_id()] = user
+        db['Users'] = users_dict
+
+        # Test codes
+        users_dict = db['Users']
+        user = users_dict[user.get_user_id()]
+        print(user.get_first_name(), user.get_last_name(), "was stored in storage.db successfully with user_id ==",user.get_user_id())
+
+        db.close()
+
+        session['user_created'] = user.get_first_name() + ' ' + user.get_last_name()
+
+        return redirect(url_for('retrieve_users'))
+    return render_template('createUser.html', form=create_user_form)
+
+@app.route('/retrieveUsers')
+def retrieve_users():
+    users_dict = {}
+    db = shelve.open('storage.db', 'r')
+    users_dict = db['Users']
+    db.close()
+
+    users_list = []
+    for key in users_dict:
+        user = users_dict.get(key)
+        users_list.append(user)
+    return render_template('retrieveUsers.html', count=len(users_list), users_list=users_list)
+
+@app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
+def update_user(id):
+    update_user_form = CreateUserForm(request.form)
+    if request.method == 'POST' and update_user_form.validate():
+        users_dict = {}
+        db = shelve.open('storage.db', 'w')
+        users_dict = db['Users']
+
+        user = users_dict.get(id)
+        user.set_first_name(update_user_form.first_name.data)
+        user.set_last_name(update_user_form.last_name.data)
+        user.set_gender(update_user_form.gender.data)
+        user.set_membership(update_user_form.membership.data)
+        user.set_remarks(update_user_form.remarks.data)
+
+        db['Users'] = users_dict
+        db.close()
+
+        session['user_updated'] = user.get_first_name() + ' ' + user.get_last_name()
+
+        return redirect(url_for('retrieve_users'))
+    else:
+        users_dict = {}
+        db = shelve.open('storage.db', 'r')
+        users_dict = db['Users']
+        db.close()
+
+        user = users_dict.get(id)
+        update_user_form.first_name.data = user.get_first_name()
+        update_user_form.last_name.data = user.get_last_name()
+        update_user_form.gender.data = user.get_gender()
+        update_user_form.membership.data = user.get_membership()
+        update_user_form.remarks.data = user.get_remarks()
+
+        return render_template('updateUser.html', form=update_user_form)
+
+@app.route('/deleteUser/<int:id>', methods=['POST'])
+def delete_user(id):
+    users_dict = {}
+    db = shelve.open('storage.db', 'w')
+    users_dict = db['Users']
+
+    user = users_dict.pop(id)
+
+    db['Users'] = users_dict
+    db.close()
+
+    session['user_deleted'] = user.get_first_name() + ' ' + user.get_last_name()
+
+    return redirect(url_for('retrieve_users'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
+
